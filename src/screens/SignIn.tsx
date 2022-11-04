726ba2b7e7ade3b6cf4e589c82267ab6d1d5315c
Guilherme Bezerra <gbsantos.it@gmail.com>
2022-11-04 13:34:56 -0300

    ✨ feat(mobile): initial screen

---
+++ src/screens/SignIn.tsx
@@ -0,0 +1,13 @@
+import { Text, Center } from "native-base";
+
+export function SignIn() {
+  return (
+    <Center flex={1}
+      alignItems="center"
+      justifyContent="center"
+      bgColor="gray.900"
+    >
+      <Text color="white" fontSize="24">SignIn</Text>
+    </Center>
+  );
+}
