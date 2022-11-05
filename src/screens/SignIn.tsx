9c231092f6e360a2b8765c1ae378f468fe04ae62
Guilherme Bezerra <gbsantos.it@gmail.com>
2022-11-05 16:12:49 -0300

    ⚙️  config(mobile): type svg

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,13 +1,18 @@
 import { Text, Center } from "native-base";
 
+import Logo from "../assets/logo.svg";
+
 export function SignIn() {
   return (
-    <Center flex={1}
+    <Center
+      flex={1}
       alignItems="center"
       justifyContent="center"
       bgColor="gray.900"
     >
-      <Text color="white" fontSize="24">SignIn</Text>
+      <Text color="white" fontSize="24">
+        <Logo height={40} width={212} />
+      </Text>
     </Center>
   );
 }
