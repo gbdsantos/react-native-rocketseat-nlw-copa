e003e4a91db88d4e38d6b893a2ae12ad4f4a0987
Guilherme Bezerra <gbsantos.it@gmail.com>
2022-11-05 16:56:31 -0300

    🎨 styles(mobile): finish SignIn screen

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,17 +1,25 @@
-import { Text, Center } from "native-base";
+import { Icon, Text, Center } from "native-base";
+import { Fontisto } from "@expo/vector-icons";
+
+import { Button } from "../components/Button";
 
 import Logo from "../assets/logo.svg";
 
 export function SignIn() {
   return (
-    <Center
-      flex={1}
-      alignItems="center"
-      justifyContent="center"
-      bgColor="gray.900"
-    >
-      <Text color="white" fontSize="24">
-        <Logo height={40} width={212} />
+    <Center flex={1} bgColor="gray.900" p={7}>
+      <Logo height={40} width={212} />
+
+      <Button
+        leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />}
+        title="ENTRAR COM O GOOGLE"
+        type="SECONDARY"
+        mt={12}
+      />
+
+      <Text color="white" mt={4} textAlign="center">
+        Não utilizamos nenhuma informação além {"\n"} do seu e-mail para criação
+        de sua conta.
       </Text>
     </Center>
   );
